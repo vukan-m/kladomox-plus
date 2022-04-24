3135761e0efad0d2b20642b7eacc2f51ccc8b375
Vukan Mirkovic <vukan.mirkovic@gmail.com>
2022-04-24 13:45:36 +0200

    Integrated Strapi

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,16 +1,33 @@
 import * as React from 'react'
 import Layout from '../components/layout'
 import Box from '../components/box'
-import Loader from '../components/loader'
+import { ImageGroup, Image } from 'react-fullscreen-image'
+import useFetch from '../components/fetch'
 import '../css/main.css'
 
 
 const GalleryPage = () => {
 
+  const fetchData = useFetch("http://localhost:1337/api/upload/files/");
+
   return (
     <Layout pageTitle="Galerija" navName="nav-links">
       <Box>
-        <Loader />
+        <div className="gallery-page">
+          <h2 className="padding">Galerija</h2>
+          <ImageGroup>
+          <ul className="images">
+              {fetchData.map(i => (
+              <li key={i?.id}>
+                  <Image
+                  src={"http://localhost:1337" + i?.formats?.large?.url}
+                  alt={i?.alternativeText}
+                  />
+              </li>
+              ))}
+          </ul>
+          </ImageGroup>
+        </div>
       </Box>
     </Layout>
   )
